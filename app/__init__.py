d9d794acfbaf728171b6d40cd4ac4b1781ab6cb3
futuresocks <colin@dogoodercomics.co.uk>
2018-06-07 13:46:34 +0100

    migrations stuff working

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
 # local imports
 from config import app_config
 
@@ -11,16 +11,19 @@ login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-    
+
     @app.route('/')
     def hello_word():
         return 'Hello World!'
+
+    migrate = Migrate(app, db)
+    from app import models
 
     return app
