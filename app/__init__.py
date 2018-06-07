30b267d34b84ee90d91836443863f183d9d2a229
futuresocks <colin@dogoodercomics.co.uk>
2018-06-07 15:38:12 +0100

    home page working

--- app/__init__.py
+++ app/__init__.py
@@ -19,11 +19,16 @@ def create_app(config_name):
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
 
-    @app.route('/')
-    def hello_word():
-        return 'Hello World!'
-
     migrate = Migrate(app, db)
     from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     return app
