ebe6587e230ace970ad268938763e52c6ef8c671
futuresocks <colin@dogoodercomics.co.uk>
2018-06-07 17:21:54 +0100

    registration, login and out all HANDLED

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootsrap
 # local imports
 from config import app_config
 
@@ -13,13 +14,15 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
-    db.init_app(app)
 
+    Bootstrap(app)
+    db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
 
     migrate = Migrate(app, db)
+
     from app import models
 
     from .admin import admin as admin_blueprint
